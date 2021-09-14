726167a9525510685c941b9fe644c4fa17fbeab1
Fernando <fernando.85@outlook.com>
2021-09-14 12:12:52 -0300

    base

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,9 @@
+class CalcController {
+
+    constructor() {
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+    }
+
+}
