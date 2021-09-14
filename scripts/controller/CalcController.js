d8481d49f418c5fc23009d3a2d9c5d8e72578787
Fernando <fernando.85@outlook.com>
2021-09-14 13:24:47 -0300

    getters and setters

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,6 +4,41 @@ class CalcController {
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#hora');
+
+        this._currentDate;
+    }
+
+
+    get displayTime() {
+        return this._timeEL.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
     }
 
 }
