9c0889c78cd06ee3d1011a1d46a2030aa5273cdb
Fernando <fernando.85@outlook.com>
2021-09-15 13:40:22 -0300

    adicionando áudio ao acionar o botão

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -14,7 +16,7 @@ class CalcController {
         this.initButtonsEvents();
         this.initKeyboard();
     }
-    
+
 
     pasteFromClipboard() {
 
@@ -56,14 +58,42 @@ class CalcController {
 
         this.setLastNumberToDisplay();
 
-        this.pasteFromClipboard()
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            })
+
+        })
 
     }
+
+
+
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff
+
+    }
+
+    playAudio() {
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+
+    }
+
 
     initKeyboard() {
 
         document.addEventListener('keyup', e => {
 
+            this.playAudio();
 
             switch (e.key) {
 
@@ -175,7 +205,15 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try {
+
+            return eval(this._operation.join(""));
+        } catch (e) {
+            setTimeout(() => {
+                this.setError();
+            }, 1)
+
+        }
 
     }
 
@@ -322,6 +360,8 @@ class CalcController {
 
     execBtn(value) {
 
+        this.playAudio();
+
         switch (value) {
 
             case 'ac':
@@ -447,6 +487,11 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return
+        }
 
         this._displayCalcEl.innerHTML = value;
 
